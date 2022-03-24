9ffa005358ef9d16c74cb5dd6ffe86b2de4758b2
thevcr <thevcr@gmail.com>
2022-03-24 18:09:59 -0400

    create question and answer elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 var timerEl = document.querySelector('.countdown');
 var mainEl = document.querySelector('.page-content');
-var startEl = document.querySelector("#start-btn");
 var questions = [];
+
 var questionCount = 0;
+var answerCount = 0;
 
 // Timer that counts down from 75
 function countdown() {
@@ -30,7 +31,6 @@ var startButtonHandler = function (event) {
     var targetEl = event.target;
   
     if (targetEl.matches("#start-btn")) {
-        debugger;
         removeStarterText();
     }
     startQuiz();
@@ -39,34 +39,61 @@ var startButtonHandler = function (event) {
 // Displays the quiz question one at a time
 function startQuiz() {
     countdown();
+    generateQuestions();
     // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var questionInterval = setInterval(function () {
-    // If there are no more words left in the message
-    if (questions[questionCount] === undefined) {
-        // Use `clearInterval()` to stop the timer
-        clearInterval(questionInterval);
-      } else {
-        // Display one word of the message
-        mainEl.textContent = question[questionCount];
-        questionCount++;
-      }
-    }, 1000);
+    // var questionInterval = setInterval(function () {
+    // // If there are no more words left in the message
+    // if (questions[questionCount] === undefined) {
+    //     // Use `clearInterval()` to stop the timer
+    //     clearInterval(questionInterval);
+    //   } else {
+    //     // Display one word of the message
+    //     generateQuestions();
+    //     mainEl.textContent = question[questionCount];
+    //     questionCount++;
+    //   }
+    // }, 1000);
 };
 
-function removeStarterText() {
+var removeStarterText = function() {
     var startTitleEl = document.querySelector("#start-title");
-    var startTextEl = document.querySelector("#start-text");
-    var startBtnEl = document.querySelector("#start-btn");
+    startTitleEl.remove();
 
-    if (mainEl.matches("#start-title")) {
-        mainEl.removeChild(startTitleEl);
-    }
-    if (mainEl.matches("#start-text")) {
-        mainEl.removeChild(startTextEl);
-    }
-    if (mainEl.matches("#start-btn")) {
-        mainEl.removeChild(startBtnEl);
-    }
+    var startTextEl = document.querySelector("#start-text");
+    startTextEl.remove();
+
+    var startBtnEl = document.querySelector("#start-btn");
+    startBtnEl.remove();
 };
+
+var generateQuestions = function() {
+    var questionContainerEl = document.createElement("div");
+    questionContainerEl.className = "questions-container";
+    mainEl.appendChild(questionContainerEl);
+
+    var questionTextEl = document.createElement("h1");
+    questionTextEl.className = "question-text";
+    questionTextEl.textContent = "Commonly used data types DO NOT include:";
+    questionContainerEl.appendChild(questionTextEl)
+
+    var answerList = document.createElement("ul");
+    answerList.className = "answer-list";
+    questionContainerEl.appendChild(answerList);
+
+    
+    for (i = 0; i < 4; i++) {
+        // create answer buttons
+        var answerListItem = document.createElement("li");
+        answerListItem.classname = "answer-list-item";
+
+        var answerButtonEl = document.createElement("button");
+        answerButtonEl.textContent = "";
+        answerButtonEl.className = "btn answer-btn";
+
+        answerList.appendChild(answerListItem)
+        answerListItem.appendChild(answerButtonEl);
+    };
+};
+
 
 mainEl.addEventListener("click", startButtonHandler);
